9ed1e60ff107f21e51bfbff9d08bc710931569c2
Helder Manuel Vieira Ruiz <manuhelderruiz@gmail.com>
2026-05-29 12:54:08 +0200

    fix: error build bundle for Vercel and code syntax UI re-styled

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,38 +1,19 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { MARKED_OPTIONS, provideMarkdown } from 'ngx-markdown';
-import { Renderer, type Tokens } from 'marked';
-import hljs from 'highlight.js';
+import { provideMarkdown } from 'ngx-markdown';
 
 import { routes } from './app.routes';
 import { jwtInterceptor } from './core/interceptors/jwt.interceptor';
 import { TranslocoHttpLoader } from './transloco-loader';
 import { provideTransloco } from '@jsverse/transloco';
 
-const markdownRenderer = new Renderer();
-
-markdownRenderer.code = ({ text, lang }: Tokens.Code) => {
-  const language = lang && hljs.getLanguage(lang) ? lang : 'plaintext';
-  const highlighted = hljs.highlight(text, { language }).value;
-  return `<pre><code class="hljs language-${language}">${highlighted}</code></pre>`;
-};
-
-markdownRenderer.html = ({ text }: Tokens.HTML) => {
-  return text.replaceAll('&', '&amp;').replaceAll('<', '&lt;').replaceAll('>', '&gt;');
-};
-
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(withInterceptors([jwtInterceptor])),
-    provideMarkdown({
-      markedOptions: {
-        provide: MARKED_OPTIONS,
-        useValue: { renderer: markdownRenderer },
-      },
-    }),
+    provideMarkdown(),
     provideTransloco({
       config: {
         availableLangs: ['en', 'es', 'fr', 'pt'],
